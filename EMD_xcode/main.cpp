fe839b8e47ac7b8b8b3934794e60267cbea7570e
agurusa <agurusa@gmail.com>
2017-01-13 13:22:30 -0800

    Initial Commit

---
+++ EMD_xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EMD_xcode
+//
+//  Created by Aarthi Gurusami on 1/13/17.
+//  Copyright © 2017 Aarthi Gurusami. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
